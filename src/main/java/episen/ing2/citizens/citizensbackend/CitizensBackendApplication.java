75c9ecbfcf2c7b0efee1c538e4a9dc85c17ea17b
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-10-05 11:50:43 +0200

    first commit, add maven dependencies

---
+++ src/main/java/episen/ing2/citizens/citizensbackend/CitizensBackendApplication.java
@@ -0,0 +1,13 @@
+package episen.ing2.citizens.citizensbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizensBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/episen/ing2/citizens/citizensbackend/CitizensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package episen.ing2.citizens.citizensbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
