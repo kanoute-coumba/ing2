fa9417142d2b54cc393bbf4bfd52b411a095d716
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-10-06 21:55:53 +0200

    [UPDATE] Repository

--- src/main/java/repository/TestRepo.java
+++ src/main/java/repository/TestRepo.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.CrudRepository;
 public class TestRepo {
 
     @Repository
-    public interface Test extends CrudRepository<Test, Test> {
+    public interface Test extends CrudRepository<Test, Integer> {
     }
 }
