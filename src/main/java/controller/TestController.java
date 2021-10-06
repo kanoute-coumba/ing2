513fad0900e4b8d972af7f9eedb811c4babd5c02
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-10-06 21:55:53 +0200

    [NEW FILE] Controller

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.TestService;
+import model.Test;
+
+@RestController
 public class TestController {
+
+    @Autowired
+    private TestService testService;
+
+    /**
+     * Read - Get all employees
+     * @return - An Iterable object of Employee full filled
+     */
+    @GetMapping("/employees")
+    public Iterable<Test> getTest() {
+        return testService.getTest();
+    }
+
 }
