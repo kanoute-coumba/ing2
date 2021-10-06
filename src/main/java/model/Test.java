56a2870aa6e35ec58db084aaa5e5af1d3fdfb301
danyzhr <danyzhr@live.fr>
2021-10-06 20:26:01 +0200

    add test model

--- src/main/java/model/Test.java
+++ src/main/java/model/Test.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Test {
+    private int Id;
+    private String name;
+
+    public Test(int id, String n) {
+        Id = id;
+        name = n;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
