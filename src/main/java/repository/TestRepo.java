8895dad57e9d752b80cff802c12d35e7b32a09d4
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-10-06 21:35:12 +0200

    [NEW FILES] Repository + Service

--- src/main/java/repository/TestRepo.java
+++ src/main/java/repository/TestRepo.java
@@ -1,4 +1,13 @@
 package repository;
 
+import model.Test;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+
+
 public class TestRepo {
+
+    @Repository
+    public interface Test extends CrudRepository<Test, Test> {
+    }
 }
--- src/main/java/service/TestService.java
+++ src/main/java/service/TestService.java
@@ -1,4 +1,33 @@
 package service;
 
+import model.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import repository.TestRepo;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
 public class TestService {
+
+    @Autowired
+    private TestRepo testRepo;
+
+    public Optional<Test> getTestId(final int id) {
+        return testRepo.findById(id);
+    }
+
+    public Iterable<Test> getTest() {
+        return testRepo.findAll();
+    }
+
+    public void deleteTestId(final int id) {
+        testRepo.deleteById(id);
+    }
+
+    public Test saveEmployee(Test test) {
+        Test savedEmployee = testRepo.save(test);
+        return savedEmployee;
+    }
+
+}
 }
