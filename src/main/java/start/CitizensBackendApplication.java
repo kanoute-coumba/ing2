e12c8c5d7d4405f32e5aa51e0b6addcefc99fbef
Amal Mimouni <mimouni.amal2000@gmail.com>
2021-10-09 00:17:42 +0200

    [UPDATE] CitizensBackendApplication

--- src/main/java/start/CitizensBackendApplication.java
+++ src/main/java/start/CitizensBackendApplication.java
@@ -1,12 +1,23 @@
 package start;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
 @SpringBootApplication
-public class CitizensBackendApplication {
+public class CitizensBackendApplication implements CommandLineRunner {
+
+	private static final Logger logger = Logger.getLogger(CitizensBackendApplication.class.getName());
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("debut de la commande");
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CitizensBackendApplication.class, args);
