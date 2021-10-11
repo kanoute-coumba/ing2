57705bbf6b96e2f9ff33118b20f8a26f9c2b994a
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-10-11 11:52:47 +0200

    add cross origin import

--- src/main/java/start/controller/TestController.java
+++ src/main/java/start/controller/TestController.java
@@ -2,6 +2,7 @@ package start.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import start.service.TestService;
