a8eae7792c3b3566518deb9d13466f53c0234615
danyzaher <dany0.zaher@etu.u-pec.fr>
2021-11-14 16:15:37 +0100

    making findby id and findall methods

--- src/main/java/episen/pds/citizens/backcitizens/repository/ConsumptionRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/ConsumptionRepo.java
@@ -3,4 +3,8 @@ package episen.pds.citizens.backcitizens.repository;
 import episen.pds.citizens.backcitizens.model.Consumption;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ConsumptionRepo extends CrudRepository<Consumption, Integer> {}
+import java.util.Optional;
+
+public interface ConsumptionRepo extends CrudRepository<Consumption, Integer> {
+    public Optional<Consumption> findConsumptionByDate(String date);
+}
--- src/main/java/episen/pds/citizens/backcitizens/service/UseMonitorService.java
+++ src/main/java/episen/pds/citizens/backcitizens/service/UseMonitorService.java
@@ -1,5 +1,8 @@
 package episen.pds.citizens.backcitizens.service;
 
+import episen.pds.citizens.backcitizens.model.Configuration;
+import episen.pds.citizens.backcitizens.model.Consumption;
+import episen.pds.citizens.backcitizens.model.Equipment;
 import episen.pds.citizens.backcitizens.model.Room;
 import episen.pds.citizens.backcitizens.repository.ConfigurationRepo;
 import episen.pds.citizens.backcitizens.repository.ConsumptionRepo;
@@ -7,6 +10,8 @@ import episen.pds.citizens.backcitizens.repository.EquipmentRepo;
 import episen.pds.citizens.backcitizens.repository.RoomRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UseMonitorService {
@@ -16,7 +21,44 @@ public class UseMonitorService {
     private ConsumptionRepo consumptionRepo;
     private RoomRepo roomRepo;
 
-    public Iterable<Room> getRoom(int id_room) {
-        roomRepo.getRoom(id_room);
+    // ROOMS
+
+    public Optional<Room> getRoomById(int id_room) {
+        return roomRepo.findById(id_room);
+    }
+    public Iterable<Room> getAllRooms() {
+        return roomRepo.findAll();
+    }
+    public void saveRoom(Room room) {
+        roomRepo.save(room);
+    }
+
+    // CONFIGURATIONS
+
+    public Optional<Configuration> getConfigurationById(int id_configuration) {
+        return configurationRepo.findById(id_configuration);
+    }
+    public Iterable<Configuration> getAllConfigurations() {
+        return configurationRepo.findAll();
+    }
+
+    // EQUIPMENT
+
+    public Optional<Equipment> getEquipmentById(int id_equipment) {
+        return equipmentRepo.findById(id_equipment);
+    }
+    public Iterable<Equipment> getAllEquipment() {
+        return equipmentRepo.findAll();
+    }
+    public void saveEquipment(Equipment equipment) {
+        equipmentRepo.save(equipment);
+    }
+    // CONSUMPTION
+
+    public Optional<Consumption> getConsumptionByDate(String date) {
+        return consumptionRepo.findConsumptionByDate(date);
+    }
+    public Iterable<Consumption> getAllConsumption() {
+        return consumptionRepo.findAll();
     }
 }
