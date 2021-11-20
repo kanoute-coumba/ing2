daa57a3fd37f694620388b8f9d23b97175d749c7
MaximeFritel <maxime.fritel@etu.u-pec.fr>
2021-11-20 11:06:19 +0100

    true request

--- src/main/java/episen/pds/citizens/backcitizens/repository/EquipmentWithConsumptionRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/EquipmentWithConsumptionRepo.java
@@ -8,6 +8,14 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface EquipmentWithConsumptionRepo extends CrudRepository<EquipmentWithConsumption,Integer> {
-    @Query(value= "select * from equipment inner join (Select max(id), value, id_equip from  consumption group by id_equip) on equipment.id=consumption.id_equip where id_room in (Select id_room from room where id_floor in (Select id_floor from floor where id_building= %:idb% )) order by consumption.value",nativeQuery = true)
+    @Query(value= "select * from equipment inner join \n" +
+            "(select * from consumption \n" +
+            "where id_consumption in (Select max \n" +
+            "from (Select max(id_consumption),id_equipment \n" +
+            "from consumption group by id_equipment) as c1)) \n" +
+            "as c2 on equipment.id_equipment=c2.id_equipment \n" +
+            "where id_room in (Select id_room from room \n" +
+            "where id_floor in (Select id_floor from floor where \n" +
+            "id_building= %:idb% )) order by c2.value",nativeQuery = true)
     public Iterable<EquipmentWithConsumption> findEquipmentByConsumption(@Param("idb") int id_building);
 }
