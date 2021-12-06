53c8f52a6e9f65a25d35e02542baf13c9234d84a
Mehdi <el-mehdi.tbili@etu.u-pec.fr>
2021-12-06 15:29:45 +0100

    implementing the floor proxy and the floor service

--- src/main/java/episen/pds/citizens/backcitizens/model/Floor.java
+++ src/main/java/episen/pds/citizens/backcitizens/model/Floor.java
@@ -1,34 +1,34 @@
-package episen.pds.citizens.backcitizens.model;
-
-import jdk.jfr.DataAmount;
-import lombok.Data;
-import org.springframework.stereotype.Component;
-import javax.persistence.*;
-
-@Entity
-@Table(name = "floor")
-public class Floor {
-    public int getIdFloor() {
-        return idFloor;
-    }
-
-    public void setIdFloor(int idFloor) {
-        this.idFloor = idFloor;
-    }
-
-    @Id
-    @Column(name="id_floor")
-    public int idFloor;
-
-    public int getIdBuilding() {
-        return idBuilding;
-    }
-
-    public void setIdBuilding(int idBuilding) {
-        this.idBuilding = idBuilding;
-    }
-
-    @Column(name="id_building")
-    public int idBuilding;
-
-}
+//package episen.pds.citizens.backcitizens.model;
+//
+//import jdk.jfr.DataAmount;
+//import lombok.Data;
+//import org.springframework.stereotype.Component;
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name = "floor")
+//public class Floor {
+//    public int getIdFloor() {
+//        return idFloor;
+//    }
+//
+//    public void setIdFloor(int idFloor) {
+//        this.idFloor = idFloor;
+//    }
+//
+//    @Id
+//    @Column(name="id_floor")
+//    public int idFloor;
+//
+//    public int getIdBuilding() {
+//        return idBuilding;
+//    }
+//
+//    public void setIdBuilding(int idBuilding) {
+//        this.idBuilding = idBuilding;
+//    }
+//
+//    @Column(name="id_building")
+//    public int idBuilding;
+//
+//}
