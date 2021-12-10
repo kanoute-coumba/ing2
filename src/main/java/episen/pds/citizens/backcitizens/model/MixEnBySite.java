e2230fba358deea9c2925cafcd9ae211a3c598bf
prisca.clio <prisca.clio@gmail.com>
2021-12-10 21:31:48 +0100

    additional column request database

--- src/main/java/episen/pds/citizens/backcitizens/model/MixEnBySite.java
+++ src/main/java/episen/pds/citizens/backcitizens/model/MixEnBySite.java
@@ -16,12 +16,23 @@ public class MixEnBySite {
     @Column(name = "name_building")
     private String name_building;
 
+    @Column(name = "active_equip")
+    private int active_equip;
+
+    @Column(name = "number_equip")
+    private int number_equip;
+
+    //@Column(name = "capacity_used")
+    //private int capacity_used;
+
     public MixEnBySite(){}
 
-    public MixEnBySite(int mix, String address, String name_building){
+    public MixEnBySite(int mix, String address, String name_building, int active_equip, int number_equip){
         this.mix=mix;
         this.address=address;
         this.name_building=name_building;
+        this.active_equip=active_equip;
+        this.number_equip=number_equip;
     }
 
     public int getMix(){
@@ -42,8 +53,9 @@ public class MixEnBySite {
     public void setName_building(String name_building){
         this.name_building=name_building;
     }
-    public String toString(){
-        return ""+address+" "+mix + " "+name_building;
-    }
+    public int getActive_equip() { return active_equip;}
+    public void setActive_equip(int active_equip) { this.active_equip = active_equip;}
+    public int getNumber_equip() { return number_equip;}
+    public void setNumber_equip(int number_equip) { this.number_equip = number_equip;}
 
 }
--- src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixBySiteRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixBySiteRepo.java
@@ -6,14 +6,18 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface CurrentMixBySiteRepo extends CrudRepository<MixEnBySite,Integer> {
 
-    @Query(value = "select mix,t1.address,name_building from " +
-            "(select sum(value) as mix, address from production p " +
-            "inner join central c on p.id_central=c.id_central " +
-            "inner join building b on c.id_building=b.id_building " +
+    @Query(value = "select mix,t2.address,t2.name_building,active_equip,number_equip from " +
+            "(select mix,t1.address,name_building,active_equip from " +
+            "(select sum(value) as mix, address, count(c.id_central) as active_equip from production p " +
+            "inner join central c on p.id_central=c.id_central inner join building b on c.id_building=b.id_building " +
             "where p.id_production in " +
-            "(select max from (select max(id_production), id_central from production group by id_central) as c1)" +
-            "and name_building in ('solaire','eolienne') " +
-            "group by address) as t1 join building b on t1.address=b.address ;", nativeQuery = true)
+            "(select max from (select max(id_production), id_central from production group by id_central) as c1) " +
+            "and name_building in ('solaire','eolienne','hydraulique') and state = 'actif' " +
+            "group by address) as t1 join building b on t1.address=b.address) as t2 join " +
+            "(select count(*) as number_equip, address " +
+            "from central c inner join building b on c.id_building=b.id_building " +
+            "where name_building in ('solaire','eolienne','hydraulique') " +
+            "group by address) as t3 on t2.address=t3.address;", nativeQuery = true)
     public Iterable<MixEnBySite> findEnergyProductionBySite();
 
 }
