8e9231d3a34e923bd50815557c72250b1ddae9f6
prisca.clio <prisca.clio@gmail.com>
2021-12-10 21:29:28 +0100

    correction in the sql request to have hydraulic energy

--- src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixRepo.java
@@ -10,7 +10,7 @@ public interface CurrentMixRepo extends CrudRepository<MixEn,Integer> {
             "inner join building b on c.id_building=b.id_building " +
             "where p.id_production in (" +
             "select max from (select max(id_production), id_central from production group by id_central) as c1)" +
-            " and name_building in ('solaire','eolienne') " +
+            " and name_building in ('solaire','eolienne','hydraulique') and state = 'actif' " +
             "group by name_building;", nativeQuery = true)
     public Iterable<MixEn> findEnergyProduction();
 
