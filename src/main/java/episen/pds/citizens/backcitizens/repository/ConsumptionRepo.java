4e17b7c2b6b6d129ddd6c178043d497c3967fce1
kanoute-coumba <kanoute.coumba@gmail.com>
2021-12-13 11:32:47 +0100

    methode to find consumpfor one daytion

--- src/main/java/episen/pds/citizens/backcitizens/repository/ConsumptionRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/ConsumptionRepo.java
@@ -8,9 +8,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ConsumptionRepo extends CrudRepository<Consumption, Integer> {
-    /* @Query(value = "select sum(value), id_building from consumption c inner join building b " +
-            "where c.id_equipment in (select id_equipment from equipment " +
-            "where id_room in (select id_room from room " +
-            "where id_floor in (select id_floor from floor))) group by id_building, date_time", nativeQuery = true)
-    public Iterable<Consumption> findWholeConsumption(); */
+    @Query(value = " select consumption.id_consumption, consumption.value, consumption.date_time, consumption.id_equipment, id_building" +
+" from consumption inner join (select * from room inner join equipment on equipment.id_room=room.id_room" +
+" inner join floor on floor.id_floor=room.id_floor)" +
+" as a1 on a1.id_equipment=consumption.id_equipment" +
+" where date_time between '2021-05-23 00:00:00' and '2021-05-23 23:59:59' order by id_equipment", nativeQuery=true)
+    public Iterable<Consumption> findConsoPerEquipementPerDay();
 }
