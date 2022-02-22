372df0316fb2e074cf84bdd88d044eac1bc1734d
kanoute-coumba <kanoute.coumba@gmail.com>
2022-02-22 16:48:57 +0100

    fix

--- src/main/java/episen/pds/citizens/backcitizens/repository/ConsumptionDayRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/ConsumptionDayRepo.java
@@ -3,6 +3,7 @@ package episen.pds.citizens.backcitizens.repository;
 import episen.pds.citizens.backcitizens.model.ConsumptionDay;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -16,8 +17,9 @@ public interface ConsumptionDayRepo extends CrudRepository<ConsumptionDay, Integ
             " in (select id_room from room where id_floor " +
             " in (select id_floor from floor where id_building " +
             " in (select id_building from building))))) " +
-            " select to_timestamp(date_time) as date, sum(value) as consoday " +
+            " select cast(date_time as date), sum(value) as consoday " +
             " from A group by (date_time)", nativeQuery=true)
 
     public Iterable<ConsumptionDay> findConsoPerEquipementPerDay();
+
 }
