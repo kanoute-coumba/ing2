a356135a788bd1a75a350033fbe11f85dc519fba
MaximeFritel <maxime.fritel@etu.u-pec.fr>
2022-04-01 19:18:36 +0200

    building by user

--- src/main/java/episen/pds/citizens/backcitizens/controller/BuildingController.java
+++ src/main/java/episen/pds/citizens/backcitizens/controller/BuildingController.java
@@ -2,13 +2,12 @@ package episen.pds.citizens.backcitizens.controller;
 
 import episen.pds.citizens.backcitizens.model.BuildingCentral;
 import episen.pds.citizens.backcitizens.model.BuildingHome;
+import episen.pds.citizens.backcitizens.model.architectureModel.Building;
 import episen.pds.citizens.backcitizens.service.BuildingCentralService;
 import episen.pds.citizens.backcitizens.service.BuildingHomeService;
+import episen.pds.citizens.backcitizens.service.architectureService.BuildingService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,7 +17,8 @@ public class BuildingController {
 
     @Autowired
     private BuildingHomeService buildingHomeService;
-
+    @Autowired
+    private BuildingService buildingService;
     @Autowired
     private BuildingCentralService buildingCentralService;
 
@@ -35,5 +35,9 @@ public class BuildingController {
     @GetMapping("/readBuildingsByDistrict")
     public List<BuildingHome> readBuildingsByDistrict(@RequestParam int district) {
         return buildingHomeService.readBuildingsByDistrict(district);
+    }
+    @GetMapping("/BuildingByUser/{idu}")
+    public Iterable<Building> getBuildingByUser(@PathVariable("idu") int idu){
+        return buildingService.getBuildingByIdUser(idu);
     }
 }
