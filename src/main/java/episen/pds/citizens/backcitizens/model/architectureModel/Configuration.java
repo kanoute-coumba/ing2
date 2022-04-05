2ce9451aaf42933c842ac021ce4aef3757d23047
Mehdi <el-mehdi.tbili@etu.u-pec.fr>
2022-04-05 10:44:52 +0200

    correcting the configuration model

--- src/main/java/episen/pds/citizens/backcitizens/model/architectureModel/Configuration.java
+++ src/main/java/episen/pds/citizens/backcitizens/model/architectureModel/Configuration.java
@@ -2,6 +2,7 @@ package episen.pds.citizens.backcitizens.model.architectureModel;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -10,25 +11,26 @@ import javax.persistence.Id;
 public class Configuration {
     @Id
     private Integer id_configuration;
-    private String date_configuration;
+    private String  date_configuration;
     private Integer id_floor;
     private Integer id_design;
     private Integer office_number;
-    private Integer openSpace_number;
-    private Integer individualSpace_number;
-    private Integer meetingRoom_number;
+    private Integer openspace_number;
+    private Integer meetingroom_number;
+    private Integer individualroom_number;
 
-    public Configuration() { }
+    public Configuration() {
+    }
 
-    public Configuration(Integer id_configuration, String date_configuration, Integer id_floor, Integer id_design, Integer office_number, Integer openSpace_number, Integer individualSpace_number, Integer meetingRoom_number) {
+    public Configuration(Integer id_configuration, String date_configuration, Integer id_floor, Integer id_design, Integer office_number, Integer openspace_number, Integer meetingroom_number, Integer individualroom_number) {
         this.id_configuration = id_configuration;
         this.date_configuration = date_configuration;
         this.id_floor = id_floor;
         this.id_design = id_design;
         this.office_number = office_number;
-        this.openSpace_number = openSpace_number;
-        this.individualSpace_number = individualSpace_number;
-        this.meetingRoom_number = meetingRoom_number;
+        this.openspace_number = openspace_number;
+        this.meetingroom_number = meetingroom_number;
+        this.individualroom_number = individualroom_number;
     }
 }
 
