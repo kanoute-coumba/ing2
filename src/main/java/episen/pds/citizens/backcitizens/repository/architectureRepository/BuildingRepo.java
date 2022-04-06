19e017c61028546466cca7f6d617a60137f90e14
MaximeFritel <maxime.fritel@etu.u-pec.fr>
2022-04-06 15:03:45 +0200

    error repair

--- src/main/java/episen/pds/citizens/backcitizens/repository/architectureRepository/BuildingRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/architectureRepository/BuildingRepo.java
@@ -3,6 +3,7 @@ package episen.pds.citizens.backcitizens.repository.architectureRepository;
 import episen.pds.citizens.backcitizens.model.architectureModel.Building;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
@@ -14,7 +15,7 @@ public interface BuildingRepo extends JpaRepository<Building, Integer> {
 
     @Query(value = "Select * from Building where id_building in (Select id_building from userbuilding where id_user=:id_u)",
     nativeQuery = true)
-    Iterable<Building> getBuildingByIdUser(@PathVariable("id_u") int idu);
+    Iterable<Building> getBuildingByIdUser(@Param("id_u") int idu);
 
 }
 
