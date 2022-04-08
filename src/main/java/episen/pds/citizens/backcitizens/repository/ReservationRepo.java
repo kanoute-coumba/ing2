6ecd6fd6d789f5eb55a8c5e89b6c36bc74c684a8
Amal Mimouni <mimouni.amal2000@gmail.com>
2022-04-08 02:16:40 +0200

    [ADD] : start reservation

---
+++ src/main/java/episen/pds/citizens/backcitizens/repository/ReservationRepo.java
@@ -0,0 +1,15 @@
+package episen.pds.citizens.backcitizens.repository;
+
+
+import episen.pds.citizens.backcitizens.model.Messages;
+import episen.pds.citizens.backcitizens.model.Reservation;
+import episen.pds.citizens.backcitizens.model.architectureModel.Space;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+    public interface ReservationRepo extends CrudRepository<Reservation, Integer> {
+
+}
