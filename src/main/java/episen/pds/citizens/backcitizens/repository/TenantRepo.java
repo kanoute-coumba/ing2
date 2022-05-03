e96c89aef737bdaabd2792e5c8da3eea0cd952f2
kanoute-coumba <kanoute.coumba@gmail.com>
2022-05-03 19:52:10 +0200

    Repositories updating

--- src/main/java/episen/pds/citizens/backcitizens/repository/TenantRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/TenantRepo.java
@@ -6,16 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 
 @Repository
 public interface TenantRepo extends CrudRepository<Tenant, Integer> {
 
+    @Query(value = "select * from tenant", nativeQuery = true)
 
-    @Query(value = " create table if not exists workplace as (select space.id_space, space.type_space, space.id_floor, a1.id_building from space inner join \n" +
-            "(select floor.id_floor, floor.id_building \n" +
-            "from floor inner join building on floor.id_building=building.id_building\n" +
-            "where building.type_building='Entreprise') as a1 on space.id_floor=a1.id_floor order by id_space) ", nativeQuery = true)
-
-    public Iterable<Tenant> getTenant();
+    public Iterable<Tenant> getAllTenant();
 
 }
