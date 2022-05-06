f6e61c83e6781474ac15fb81928c74b0cbab1acd
Mehdi <el-mehdi.tbili@etu.u-pec.fr>
2022-05-06 16:16:47 +0200

    Implementing the test of acceptance

--- src/main/java/episen/pds/citizens/backcitizens/repository/architectureRepository/ConfigurationRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/architectureRepository/ConfigurationRepo.java
@@ -4,6 +4,7 @@ import episen.pds.citizens.backcitizens.model.architectureModel.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.sql.Date;
 import java.util.List;
 
 
@@ -11,5 +12,8 @@ public interface ConfigurationRepo extends JpaRepository<Configuration, Integer>
     @Query(value = "select * from configuration ORDER BY id_configuration", nativeQuery = true)
     List<Configuration> getAllConfigurations();
 
+    @Query("SELECT CASE WHEN COUNT(c) > 0 THEN TRUE ELSE FALSE END FROM Configuration c WHERE c.id_configuration = ?1")
+    Boolean selectExistsConfiguration(Integer id_configuration);
 }
+
 
