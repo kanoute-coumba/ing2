9fe72bd09eaacca337af5600a81c3152787b5141
prisca.clio <prisca.clio@gmail.com>
2022-05-08 22:27:42 +0200

    correction in sql queries

--- src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixBySiteRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixBySiteRepo.java
@@ -8,11 +8,11 @@ public interface CurrentMixBySiteRepo extends CrudRepository<MixEnBySite,Integer
 
     @Query(value = "select mix,t2.address,t2.type_building,active_equip,number_equip from " +
             "(select mix,t1.address,type_building,active_equip from " +
-            "(select sum(value) as mix, address, count(c.id_central) as active_equip from production p " +
+            "(select sum(value) as mix, address, count(c.id_central) as active_equip from current_production p " +
             "inner join central c on p.id_central=c.id_central inner join building b on c.id_building=b.id_building " +
             "where p.id_production in " +
-            "( select p1.id_production from production p1 " +
-            "where date_time = (select max(date_time) from production p2 group by p2.id_central having p1.id_central = p2.id_central)) " +
+            "( select p1.id_production from current_production p1 " +
+            "where date_time = (select max(date_time) from current_production p2 group by p2.id_central having p1.id_central = p2.id_central)) " +
             "and type_building in ('solaire','eolienne','hydraulique') and state = 'actif' " +
             "group by address) as t1 join building b on t1.address=b.address) as t2 join " +
             "(select count(*) as number_equip, address " +
--- src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixRepo.java
+++ src/main/java/episen/pds/citizens/backcitizens/repository/CurrentMixRepo.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CurrentMixRepo extends CrudRepository<MixEn,Integer> {
-    @Query(value="select sum(value) as mix, type_building from production p " +
+    @Query(value="select sum(value) as mix, type_building from current_production p " +
             "inner join central c on p.id_central=c.id_central " +
             "inner join building b on c.id_building=b.id_building " +
             "where p.id_production in (" +
-            "select p.id_production from production p " +
-            "where date_time = (select max(date_time) from production p2 group by p2.id_central having p.id_central = p2.id_central))" +
+            "select p.id_production from current_production p " +
+            "where date_time = (select max(date_time) from current_production p2 group by p2.id_central having p.id_central = p2.id_central))" +
             " and type_building in ('solaire','eolienne','hydraulique') and state = 'actif' " +
             "group by type_building;", nativeQuery = true)
     public Iterable<MixEn> findEnergyProduction();
